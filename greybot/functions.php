723f2972294b3a1d28bf85960f3aa81bd5ca8d9c
Grey <greymcgowan@gmail.com>
2018-01-07 23:15:40 +1100

    installed google fonts (courgette & news cycle)

--- greybot/functions.php
+++ greybot/functions.php
@@ -160,3 +160,6 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+function add_google_fonts() {
+		wp_enqueue_style( 'google_fonts', 'https://fonts.googleapis.com/css?family=Courgette|News+Cycle' )
+		add_action ('wp_enqueue_scripts', 'add_google_fonts' );
